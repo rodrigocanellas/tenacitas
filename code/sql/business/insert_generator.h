2c6e7ac3dc1172d47899ed9862b4ea6baca7b02d
Canellas <rodrigo.canellas@gmail.com>
2019-11-21 17:49:31 -0300

    adjusting class model to 'insert' generation

--- code/sql/business/insert_generator.h
+++ code/sql/business/insert_generator.h
@@ -1,14 +1,14 @@
 #ifndef CAPEMISA_TEST_AUTOMATION_BUSINESS_generate_table_insert_H
 #define CAPEMISA_TEST_AUTOMATION_BUSINESS_generate_table_insert_H
 
-#include <sql/entities/insert_definition.h>
-#include <sql/entities/inserts_definitions.h>
 #include <sql/entities/column.h>
 #include <sql/entities/columns.h>
 #include <sql/entities/database.h>
 #include <sql/entities/databases.h>
 #include <sql/entities/host.h>
 #include <sql/entities/hosts.h>
+#include <sql/entities/insert_definition.h>
+#include <sql/entities/inserts_definitions.h>
 #include <sql/entities/name.h>
 #include <sql/entities/server.h>
 #include <sql/entities/servers.h>
@@ -26,8 +26,8 @@ using namespace entities;
 
 struct insert_generator
 {
-  std::string operator()(const hosts& p_hosts
-                         , const inserts_definitions & p_inserts_definitions)
+  std::string operator()(const hosts& p_hosts,
+                         const inserts_definitions& p_inserts_definitions)
   {
     std::stringstream _stream;
 
@@ -40,38 +40,92 @@ struct insert_generator
   }
 
 private:
+  struct column_created
+  {
+    column_name m_column_name;
+    std::string m_value;
+  };
 
-  std::string generate_table_insert(const hosts p_hosts
-                              , const insert_definition & p_insert_definition) {
+  struct foreign_key_created
+  {
+    typedef std::list<column_created> columns_created;
 
+    ptr<foreign_key> m_foreign_key;
+    columns_created m_columns_created;
+  };
 
-        ptr<table> _table = find_table(p_hosts, p_insert_definition);
+  typedef std::list<foreign_key_created> foreigners_keys_created;
 
-        if (_table==nullptr) {
-          return "";
-        }
+  std::string generate_table_insert(
+    const hosts p_hosts,
+    const insert_definition& p_insert_definition)
+  {
 
-        std::stringstream _stream;
+    ptr<table> _table = find_table(p_hosts, p_insert_definition);
 
-        uint16_t _amount_of_rows = p_insert_definition.get_amount_of_rows();
+    if (_table == nullptr) {
+      return "";
+    }
 
-        for(uint16_t _row = 0; _row < _amount_of_rows; ++_row) {
-          _stream << generate_row_insert(_table, _row, p_insert_definition) << std::endl;
-        }
-
-        return _stream.str();
-  }
-
-  std::string generate_row_insert(ptr<table> p_table
-                                  , uint16_t p_row
-                                  , const insert_definition & p_insert_definition) {
     std::stringstream _stream;
 
-        return _stream.str();
+    foreigners_keys_created _foreigners_keys_created;
+
+    _stream << generate_inserts_fks(
+      _table, p_insert_definition, _foreigners_keys_created);
+
+    uint16_t _amount_of_rows = p_insert_definition.get_amount_of_rows();
+
+    for (uint16_t _row = 0; _row < _amount_of_rows; ++_row) {
+      _stream << generate_row_insert(_table, _row, p_insert_definition)
+              << std::endl;
+    }
+
+    return _stream.str();
   }
 
+  std::string generate_inserts_fks(
+    ptr<table> p_table,
+    const insert_definition& p_insert_definition,
+    foreigners_keys_created& p_foreigners_keys_created)
+  {
+    std::stringstream _stream;
+    uint16_t _num_fks = p_table->get_num_fks();
+    for (uint16_t _count_fk = 0; _count_fk < _num_fks; ++_count_fk) {
+      ptr<foreign_key> _foreign_key = p_table->get_foregign_key(_count_fk);
 
-  //  std::string generate_table_insert_for_table(table* p_table, uint16_t p_num_lines)
+      foreigners_keys_values::const_iterator _foreigners_keys_values_ite =
+        p_insert_definition.get_foreigner_key_value(_foreign_key->get_name());
+
+      _stream << generate_insert_fk(_foreign_key,
+                                    *_foreigners_keys_values_ite,
+                                    p_foreigners_keys_created)
+              << std::endl;
+    }
+    return _stream.str();
+  }
+
+  std::string generate_insert_fk(
+    ptr<foreign_key> /*p_fk*/,
+    const foreign_key_value& /*p_fk_value*/,
+    foreigners_keys_created& /*p_foreigners_keys_created*/)
+  {
+    std::stringstream _stream;
+    return _stream.str();
+  }
+
+  std::string generate_row_insert(
+    ptr<table> /*p_table*/,
+    uint16_t /*p_row*/,
+    const insert_definition& /*p_insert_definition*/)
+  {
+    std::stringstream _stream;
+
+    return _stream.str();
+  }
+
+  //  std::string generate_table_insert_for_table(table* p_table, uint16_t
+  //  p_num_lines)
   //  {
   //    std::stringstream _stream;
 
@@ -86,32 +140,33 @@ private:
   //    return _stream.str();
   //  }
 
-    ptr<table> find_table(const hosts& p_hosts,
-                          const insert_definition& p_insert_definition) const
-    {
-      const host_name& _host_name = p_insert_definition.get_host_name();
-      ptr<host> _host =
-        p_hosts.find([&_host_name](const ptr<host>& p_host) -> bool {
-          return p_host->get_name() == _host_name;
-        });
+  ptr<table> find_table(const hosts& p_hosts,
+                        const insert_definition& p_insert_definition) const
+  {
+    const host_name& _host_name = p_insert_definition.get_host_name();
+    ptr<host> _host =
+      p_hosts.find([&_host_name](const ptr<host>& p_host) -> bool {
+        return p_host->get_name() == _host_name;
+      });
 
-      if (_host == nullptr) {
-        return ptr<table>();
-      }
-
-      ptr<server> _server = _host->find(p_insert_definition.get_server_name());
-      if (_server == nullptr) {
-        return ptr<table>();
-      }
-
-      ptr<database> _database = _server->find(p_insert_definition.get_database_name());
-      if (_database == nullptr) {
-        return ptr<table>();
-      }
-
-      ptr<table> _table = _database->find(p_insert_definition.get_table_name());
-      return _table;
+    if (_host == nullptr) {
+      return ptr<table>();
     }
+
+    ptr<server> _server = _host->find(p_insert_definition.get_server_name());
+    if (_server == nullptr) {
+      return ptr<table>();
+    }
+
+    ptr<database> _database =
+      _server->find(p_insert_definition.get_database_name());
+    if (_database == nullptr) {
+      return ptr<table>();
+    }
+
+    ptr<table> _table = _database->find(p_insert_definition.get_table_name());
+    return _table;
+  }
 };
 
 } // namespace business
--- code/sql/entities/insert_definition.h
+++ code/sql/entities/insert_definition.h
@@ -5,19 +5,17 @@
 #include <list>
 #include <utility>
 
-
-#include <sql/entities/names.h>
-#include <sql/entities/foreign_key_value.h>
 #include <sql/entities/column_value.h>
-#include <sql/entities/foreign_key_value_pattern.h>
 #include <sql/entities/column_value_pattern.h>
 #include <sql/entities/columns_values.h>
+#include <sql/entities/foreign_key_value.h>
+#include <sql/entities/foreign_key_value_pattern.h>
 #include <sql/entities/foreigners_keys_values.h>
+#include <sql/entities/names.h>
 
 namespace capemisa {
 namespace sql {
 namespace entities {
-
 
 struct insert_definition
 {
@@ -36,50 +34,66 @@ struct insert_definition
     , m_amount_of_rows(p_amount_of_rows)
   {}
 
-  inline columns_values::const_iterator
-  add_column_value(const column_name & p_column_name
-                   , column_value_pattern p_column_value_pattern) {
+  inline columns_values::const_iterator add_column_value(
+    const column_name& p_column_name,
+    column_value_pattern p_column_value_pattern)
+  {
     return m_columns_values.add(p_column_name, p_column_value_pattern);
   }
 
-  inline foreigners_keys_values::const_iterator
-  add_foreign_key_value(const foreign_key_name & p_foreign_key_name
-                        , foreign_key_value_pattern p_foreign_key_value_pattern
-                        , column_value_pattern p_column_value_pattern) {
-    return m_foreigners_keys_values.add(p_foreign_key_name
-                                        , p_foreign_key_value_pattern
-                                        , p_column_value_pattern);
+  inline foreigners_keys_values::const_iterator add_foreign_key_value(
+    const foreign_key_name& p_foreign_key_name,
+    foreign_key_value_pattern p_foreign_key_value_pattern,
+    column_value_pattern p_column_value_pattern)
+  {
+    return m_foreigners_keys_values.add(
+      p_foreign_key_name, p_foreign_key_value_pattern, p_column_value_pattern);
   }
 
-  inline const host_name &get_host_name()const{return m_host_name;}
-  inline const server_name &get_server_name ()const{return m_server_name;}
-  inline const database_name &get_database_name()const{return m_database_name;}
-  inline const table_name &get_table_name()const{return m_table_name;}
-  inline uint16_t get_amount_of_rows()const{return m_amount_of_rows;}
+  inline const host_name& get_host_name() const { return m_host_name; }
+  inline const server_name& get_server_name() const { return m_server_name; }
+  inline const database_name& get_database_name() const
+  {
+    return m_database_name;
+  }
+  inline const table_name& get_table_name() const { return m_table_name; }
+  inline uint16_t get_amount_of_rows() const { return m_amount_of_rows; }
 
-  inline columns_values::const_iterator begin_column_value()const {
+  inline columns_values::const_iterator begin_column_value() const
+  {
     return m_columns_values.begin();
   }
 
-  inline columns_values::const_iterator end_column_value()const {
+  inline columns_values::const_iterator end_column_value() const
+  {
     return m_columns_values.end();
   }
 
-  inline const foreigners_keys_values::const_iterator begin_foreign_key_value()const {
+  inline const foreigners_keys_values::const_iterator begin_foreign_key_value()
+    const
+  {
     return m_foreigners_keys_values.begin();
   }
 
-  inline const foreigners_keys_values::const_iterator end_foreign_key_value()const {
+  inline const foreigners_keys_values::const_iterator end_foreign_key_value()
+    const
+  {
     return m_foreigners_keys_values.end();
   }
 
-  bool operator==(const insert_definition & p_insert_definition) const {
-    return ( (m_host_name == p_insert_definition.m_host_name) &&
-             (m_server_name == p_insert_definition.m_server_name) &&
-             (m_database_name == p_insert_definition.m_database_name) &&
-             (m_table_name == p_insert_definition.m_table_name) );
+  bool operator==(const insert_definition& p_insert_definition) const
+  {
+    return ((m_host_name == p_insert_definition.m_host_name) &&
+            (m_server_name == p_insert_definition.m_server_name) &&
+            (m_database_name == p_insert_definition.m_database_name) &&
+            (m_table_name == p_insert_definition.m_table_name));
   }
 
+  foreigners_keys_values::const_iterator get_foreigner_key_value(
+    const foreign_key_name& p_fk_name) const
+  {
+    return m_foreigners_keys_values.find(p_fk_name);
+  }
 
 private:
   host_name m_host_name;
@@ -90,7 +104,6 @@ private:
   columns_values m_columns_values;
   foreigners_keys_values m_foreigners_keys_values;
 };
-
 
 } // namespace business
 } // namespace sql
