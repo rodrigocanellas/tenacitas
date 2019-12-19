c6be7421589130d275993bc1e17141ed48014b0d
rodrigocanellas <rodrigo.canellas@gmail.com>
2019-12-19 13:04:12 -0300

    testing table traversal

--- code/sql/test/generators/main.cpp
+++ code/sql/test/generators/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <utility>
 
+#include <sql/business/traverse_tables.h>
 #include <sql/business/number_value_generator.h>
 #include <sql/business/one_pk_all_fks.h>
 #include <sql/business/sql_generator.h>
@@ -48,7 +49,7 @@ attribute_sequential()
   typedef number_value_generator<time_t, attribute_column> birthday_generator;
 
   ptr<birthday_generator> _asvg(make_ptr<birthday_generator>(
-    _now, _now + 30 * day_in_secs, 2 * day_in_secs));
+                                  _now, _now + 30 * day_in_secs, 2 * day_in_secs));
 
   ptr<column_values> _values = (*_asvg)(_birthday, 8);
 
@@ -77,7 +78,7 @@ pk_sequential()
   ptr<database> _db = _server->get_database(0);
   ptr<table> _employee = _db->find("employee");
   ptr<primary_key_column> _id =
-    _employee->get_primary_key()->find_pk_column("id");
+      _employee->get_primary_key()->find_pk_column("id");
 
   typedef number_value_generator<int32_t, primary_key_column> id_generator;
 
@@ -124,16 +125,16 @@ fk_one_pk()
     _employee_gen.add_pk_generator("id", make_ptr<id_generator>(100, -20, -5));
 
     _employee_gen.add_attr_generator(
-      "name", make_ptr<name_generator>("employee", 30, 90));
+          "name", make_ptr<name_generator>("employee", 30, 90));
 
     _employee_gen.add_attr_generator(
-      "birthday", make_ptr<birthday_generator>(time(nullptr), 2 * day_in_secs));
+          "birthday", make_ptr<birthday_generator>(time(nullptr), 2 * day_in_secs));
 
     ptr<const table_values> _employee_pk_values =
-      _employee_gen.generate_pks(_all_pks);
+        _employee_gen.generate_pks(_all_pks);
 
     ptr<const table_values> _employee_attrs_values =
-      _employee_gen.generate_attrs();
+        _employee_gen.generate_attrs();
 
     std::cout << _sql_gen(_employee_pk_values,
                           _employee_attrs_values,
@@ -148,23 +149,23 @@ fk_one_pk()
 
     time_t _now = time(nullptr);
     _dependent_gen.add_attr_generator(
-      "birthday",
-      make_ptr<birthday_generator>(
-        _now, _now + 25 * day_in_secs, 5 * day_in_secs));
+          "birthday",
+          make_ptr<birthday_generator>(
+            _now, _now + 25 * day_in_secs, 5 * day_in_secs));
 
     _dependent_gen.add_fk_generator("employee_fk", make_ptr<one_pk_all_fks>());
 
     ptr<const table_values> _dependent_pk_values =
-      _dependent_gen.generate_pks(_all_pks);
+        _dependent_gen.generate_pks(_all_pks);
     ptr<const table_values> _dependent_attrs_values =
-      _dependent_gen.generate_attrs();
+        _dependent_gen.generate_attrs();
 
     std::pair<ptr<table_values>, table_generator::tables_names_ptr> _result =
-      _dependent_gen.generate_fks(_all_pks);
+        _dependent_gen.generate_fks(_all_pks);
 
     if (_result.first) {
       std::cout << _sql_gen(
-        _dependent_pk_values, _dependent_attrs_values, _result.first);
+                     _dependent_pk_values, _dependent_attrs_values, _result.first);
     } else {
       std::cout << "Dados para seguintes tabelas devem ser geradas primeiro: ";
       for (const name& _name : *_result.second) {
@@ -205,23 +206,23 @@ not_all_tables_generated_1()
 
   time_t _now = time(nullptr);
   _dependent_gen.add_attr_generator(
-    "birthday",
-    make_ptr<birthday_generator>(
-      _now, _now + 25 * day_in_secs, 5 * day_in_secs));
+        "birthday",
+        make_ptr<birthday_generator>(
+          _now, _now + 25 * day_in_secs, 5 * day_in_secs));
 
   _dependent_gen.add_fk_generator("employee_fk", make_ptr<one_pk_all_fks>());
 
   ptr<const table_values> _dependent_pk_values =
-    _dependent_gen.generate_pks(_all_pks);
+      _dependent_gen.generate_pks(_all_pks);
   ptr<const table_values> _dependent_attrs_values =
-    _dependent_gen.generate_attrs();
+      _dependent_gen.generate_attrs();
 
   std::pair<ptr<table_values>, table_generator::tables_names_ptr> _result =
-    _dependent_gen.generate_fks(_all_pks);
+      _dependent_gen.generate_fks(_all_pks);
 
   if (_result.first) {
     std::cout << _sql_gen(
-      _dependent_pk_values, _dependent_attrs_values, _result.first);
+                   _dependent_pk_values, _dependent_attrs_values, _result.first);
   } else {
     std::cout << "Dados para seguintes tabelas devem ser gerados primeiro:\n ";
     for (const name& _name : *_result.second) {
@@ -284,31 +285,66 @@ abstract_1()
   table_generator _tb(_a, 5);
 
   _tb.add_pk_generator(
-    "id",
-    make_ptr<number_value_generator<uint16_t, primary_key_column>>(4, 50, 2));
+        "id",
+        make_ptr<number_value_generator<uint16_t, primary_key_column>>(4, 50, 2));
 
   _tb.add_attr_generator(
-    "a1",
-    make_ptr<number_value_generator<uint16_t, attribute_column>>(
-      1000.00, 1600.00, 250.00));
+        "a1",
+        make_ptr<number_value_generator<uint16_t, attribute_column>>(
+          1000.00, 1600.00, 250.00));
 
   generic::ptr<entities::tables_values> _all_pks(
-    make_ptr<entities::tables_values>());
+        make_ptr<entities::tables_values>());
 
   _tb.generate_pks(_all_pks);
   _tb.generate_attrs();
 
   std::pair<ptr<table_values>, table_generator::tables_names_ptr> _result =
-    _tb.generate_fks(_all_pks);
+      _tb.generate_fks(_all_pks);
 
   if (_result.first != nullptr) {
     throw std::runtime_error(
-      "FKs para 'A' geradas, mas 'D' e 'B' não foram ṕopuladas");
+          "FKs para 'A' geradas, mas 'D' e 'B' não foram ṕopuladas");
   }
 
   cout << "As tabelas abaixo devem ser povoadas: " << endl;
   for (const name& _name : *_result.second) {
     cout << _name << endl;
+  }
+}
+
+void
+abstract_2()
+{
+  using namespace capemisa::sql::entities;
+  using namespace capemisa::sql::generic;
+  using namespace capemisa::sql::business;
+  using namespace capemisa::sql::test;
+  using namespace std;
+
+  cout << "\nABSTRACT 2" << endl;
+
+  create_abstract_model _cam;
+
+  ptr<hosts> _hosts = _cam();
+  ptr<host> _host = (*_hosts)[0];
+  ptr<server> _server = _host->get_server(0);
+  ptr<database> _db = _server->get_database(0);
+
+  ptr<table> _table = _db->find("E");
+
+  if (_table == nullptr) {
+    throw std::runtime_error("não foi possível achar a tabela 'E'");
+  }
+
+  std::list<name> _names;
+  traverse_tables _tt;
+  _tt(_table.get(), [&_names](const table * const p_table) -> void {
+    _names.push_front(p_table->get_name());
+  });
+
+  for (const name & _name : _names) {
+    std::cout << _name << std::endl;
   }
 }
 
@@ -321,4 +357,5 @@ main()
   text_generator();
   not_all_tables_generated_1();
   abstract_1();
+  abstract_2();
 }
