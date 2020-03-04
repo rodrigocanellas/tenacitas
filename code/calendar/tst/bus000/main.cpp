7385e25d3c4ad21b5c8956a0a0e5e688576e243c
rodrigocanellas <rodrigo.canellas@gmail.com>
2020-03-04 17:52:35 -0300

    trying to use gregorian calendar

--- code/calendar/tst/bus000/main.cpp
+++ code/calendar/tst/bus000/main.cpp
@@ -18,7 +18,20 @@
 
 using namespace tenacitas::calendar::ent;
 using namespace tenacitas::calendar::bus;
-
+/*
+// day of the week for january 1st
+#include <iostream>
+using namespace std;
+int main ()
+{
+  char*week[]={"sun","mon","tue","wed","thu","fri","sat"};
+  int year,days;
+  cout<< "year?"; cin>>year;
+  days = 365*year + (year-1)/4 - (year-1)/100 + (year-1)/400 ;
+  cout<<week[days%7]<<"\n";
+  return 0;
+}
+*/
 
 int
 main()
