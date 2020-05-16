7a036c56c0574338d5d374d330b16a36953cffdf
rodrigocanellas <rodrigo.canellas@gmail.com>
2020-05-16 19:04:33 -0300

    removing unecessary files

--- tst/concurrent/performance/thread_pool_001/main.cpp
+++
@@ -1,4 +0,0 @@
-#include <concurrent/_tst/performance/thread_pool_tester.h>
-#include <concurrent/thread_pool.h>
-#include <logger/cerr/log.h>
-#include <tester/test.h>
--- tst/concurrent/performance/thread_pool_004/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <concurrent/_tst/performance/thread_pool_tester.h>
-#include <concurrent/thread_pool.h>
-#include <logger/cerr/log.h>
-#include <tester/test.h>
-
-int main(int argc, char **argv) {
-  logger::cerr::log::set_debug();
-  tester::_bus::test::run<thread_pool_004>(argc, argv);
-}
