b354a16cc6ff3ce118d2b75a2e614fb0a9566957
rodrigocanellas <rodrigo.canellas@gmail.com>
2020-08-20 18:39:43 -0300

    new test for 'producer_consumer'

--- tst/concurrent/producer_consumer/main.cpp
+++ tst/concurrent/producer_consumer/main.cpp
@@ -70,36 +70,35 @@ struct producer_consumer_001 {
     concurrent_log_debug(logger::cerr::log, "capacity = ", _pc.capacity(),
                          ", occupied = ", _pc.occupied());
 
-    worker _work;
-
-    msg::data _value = 0;
-
     sleeping_loop _loop(
         std::chrono::milliseconds(500),
-        [&_pc, &_value]() {
-          msg _msg(++_value);
+        [&_pc, this]() {
+          msg _msg(++m_value);
           concurrent_log_debug(log, "going to add ", _msg);
 
           _pc.add(std::move(_msg));
           concurrent_log_debug(logger::cerr::log, "added msg ", _msg,
                                "; capacity = ", _pc.capacity(),
                                ", occupied = ", _pc.occupied());
+
           return status::ok;
         },
         std::chrono::milliseconds(300));
 
     _pc.add(
-        [&_work](msg &&p_msg) -> status::result {
-          return _work(std::move(p_msg));
+        [this](msg &&p_msg) -> status::result {
+          return m_work(std::move(p_msg));
         },
         std::chrono::minutes(1));
 
     _pc.start();
     _loop.start();
 
-    concurrent_log_debug(logger::cerr::log, "sleeping for 10 secs");
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    concurrent_log_debug(logger::cerr::log, "waking up after 10 secs");
+    concurrent_log_debug(logger::cerr::log, "sleeping for 20 secs");
+    std::this_thread::sleep_for(std::chrono::seconds(20));
+    concurrent_log_debug(logger::cerr::log, "waking up after 20 secs");
+
+    _loop.stop();
 
     //    concurrent_log_debug(logger::cerr::log, "stopping the pool");
     //    _pc.stop();
@@ -115,16 +114,18 @@ struct producer_consumer_001 {
     //    std::this_thread::sleep_for(std::chrono::seconds(4));
     //    concurrent_log_debug(logger::cerr::log, "waking up after 4 secs");
 
-    //    concurrent_log_debug(logger::cerr::log, "provided = ", _value,
-    //                         ", consumed = ", _work.m_msg.counter());
-    //    if (_work.m_msg.counter() != _value) {
-    //      concurrent_log_error(logger::cerr::log,
-    //                           "Data value consumed should be equal to
-    //                           provided");
-    //      return false;
-    //    }
+    std::this_thread::sleep_for(std::chrono::seconds(2));
 
     return true;
+  }
+
+  ~producer_consumer_001() {
+    concurrent_log_debug(logger::cerr::log, "provided = ", m_value,
+                         ", consumed = ", m_work.m_msg.counter());
+    if (m_work.m_msg.counter() != m_value) {
+      concurrent_log_error(logger::cerr::log,
+                           "Data value consumed should be equal to provided");
+    }
   }
 
   static std::string desc() {
@@ -153,12 +154,17 @@ private:
                            &p_msg);
 
       concurrent_log_debug(log, "worker is going to sleep");
-      std::this_thread::sleep_for(std::chrono::seconds(5));
+      std::this_thread::sleep_for(std::chrono::seconds(1));
       concurrent_log_debug(log, "worker is waking up");
       return status::ok;
     }
     msg m_msg;
   };
+
+private:
+  worker m_work;
+
+  msg::data m_value = 0;
 };
 
 int main(int argc, char **argv) {
