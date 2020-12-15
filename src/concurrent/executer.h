3d156147915a63fd372895e8cff22ba96b2e41fa
rodrigocanellas <rodrigo.canellas@gmail.com>
2020-12-14 21:07:00 -0300

    removing 'executer' with no timeout

--- src/concurrent/executer.h
+++ src/concurrent/executer.h
@@ -33,162 +33,9 @@ namespace tenacitas {
 /// \brief namespace of the project
 namespace concurrent {
 
-enum class timeout_control : char { yes = 'y', no = 'n' };
-
 template <typename t_log> struct executer_base_t {
-protected:
-  executer_base_t(std::function<void()> p_function) : m_function(p_function) {}
 
-  virtual ~executer_base_t() {}
-
-protected:
-  std::function<void()> m_function;
-
-  /// \brief Logger object
-  t_log m_log{"executer"};
-
-  /// \brief The loop initiates stopped
-  bool m_stopped{true};
-
-  /// \brief Controls the execution of the execution loop
-  std::mutex m_mutex_function;
-
-  /// \brief Controls the execution of the execution loop
-  std::condition_variable m_cond_exec;
-
-  /// \brief Controls the waiting of the execution loop
-  std::mutex m_mutex_wait;
-
-  /// \brief Controls the waiting of the execution loop
-  std::condition_variable m_cond_wait;
-
-  /// \brief Single thread used to execute the \p function over and over
-  /// asynchronously
-  concurrent::thread m_thread;
-
-  /// \brief Controls the execution of the thread
-  std::mutex m_mutex_exec;
-};
-
-template <typename t_log>
-struct executer_no_timeout_t : protected executer_base_t<t_log> {
-
-  executer_no_timeout_t(const executer_no_timeout_t &) = delete;
-  executer_no_timeout_t(executer_no_timeout_t &&) = delete;
-  executer_no_timeout_t &operator=(const executer_no_timeout_t &) = delete;
-  executer_no_timeout_t &operator=(executer_no_timeout_t &&) = delete;
-
-  virtual ~executer_no_timeout_t() {
-    concurrent_debug(this->m_log, "entering destructor");
-    stop();
-    concurrent_debug(this->m_log, "leaving destructor");
-  }
-
-protected:
-  executer_no_timeout_t(std::function<void()> p_function)
-      : executer_base_t<t_log>(p_function) {
-    start();
-  }
-
-  /// \brief Stars the single thread that will execute the function
-  /// asynchronously
-  void start() {
-    if (this->m_stopped) {
-      concurrent_debug(this->m_log, "starting");
-      this->m_stopped = false;
-      this->m_thread = concurrent::thread([this]() -> void { looping(); });
-      std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    }
-  }
-
-  /// \brief Stops the execution of the single created thread for the execution
-  /// of the function asynchronously
-  void stop() {
-    if (!this->m_stopped) {
-      concurrent_debug(this->m_log, "stopping");
-      this->m_stopped = true;
-      concurrent_debug(this->m_log, "m_stopped = true");
-      this->m_cond_exec.notify_one();
-      concurrent_debug(this->m_log, "m_cond_exec.notify_one()");
-      this->m_cond_wait.notify_one();
-      concurrent_debug(this->m_log, "m_cond_wait.notify_one()");
-      this->m_thread.join();
-      concurrent_debug(this->m_log, "m_thread.join()");
-    } else {
-      concurrent_debug(this->m_log,
-                       "not stopping because it was already stopped");
-    }
-  }
-
-  template <typename t_result>
-  t_result call_no_timeout(std::function<t_result()> p_ok,
-                           std::function<t_result()> p_not_ok,
-                           std::function<void()> p_save_params) {
-    std::lock_guard<std::mutex> _lock_function(this->m_mutex_function);
-    concurrent_debug(this->m_log, "operator()()");
-
-    if (this->m_stopped) {
-      concurrent_warn(this->m_log, "executer is stopped; call 'start()' first");
-      return p_not_ok();
-    }
-    p_save_params();
-
-    concurrent_debug(this->m_log, "notifying there is work to be done");
-    this->m_cond_exec.notify_one();
-
-    concurrent_debug(this->m_log, "waiting for work to be done");
-    std::unique_lock<std::mutex> _lock(this->m_mutex_wait);
-    this->m_cond_wait.wait(_lock);
-
-    concurrent_debug(this->m_log, "work was done or 'executer' was stopped");
-    if (this->m_stopped) {
-      concurrent_debug(this->m_log, "stopped");
-      return p_not_ok();
-    }
-
-    concurrent_debug(this->m_log, "function done");
-    return p_ok();
-  }
-
-private:
-  void looping() {
-    while (true) {
-
-      concurrent_debug(this->m_log, "waiting for work");
-
-      std::unique_lock<std::mutex> _lock(this->m_mutex_exec);
-      this->m_cond_exec.wait(_lock);
-
-      if (this->m_stopped) {
-        concurrent_debug(this->m_log, "stopped");
-        this->m_cond_wait.notify_one();
-        concurrent_debug(this->m_log, "about to break the loop");
-        break;
-      }
-
-      concurrent_debug(this->m_log, "function to execute!");
-
-      this->m_function();
-
-      if (this->m_stopped) {
-        concurrent_debug(this->m_log, "stopped while in function");
-        this->m_cond_wait.notify_one();
-        break;
-      }
-
-      concurrent_debug(this->m_log, "notifying that work is done");
-      this->m_cond_wait.notify_one();
-      concurrent_debug(this->m_log, "notification sent");
-    }
-
-    concurrent_debug(this->m_log, "leaving the loop");
-  }
-};
-
-template <typename t_log>
-struct executer_with_timeout_t : protected executer_base_t<t_log> {
-
-  virtual ~executer_with_timeout_t() {
+  virtual ~executer_base_t() {
     concurrent_debug(this->m_log, "entering destructor");
     stop();
     concurrent_debug(this->m_log, "leaving destructor");
@@ -196,9 +43,9 @@ struct executer_with_timeout_t : protected executer_base_t<t_log> {
 
 protected:
   template <typename t_time>
-  executer_with_timeout_t(std::function<void()> p_function, t_time p_timeout,
-                          timeout_callback p_timeout_callback)
-      : executer_base_t<t_log>(p_function), m_timeout(to_timeout(p_timeout)),
+  executer_base_t(std::function<void()> p_function, t_time p_timeout,
+                  timeout_callback p_timeout_callback)
+      : m_function(p_function), m_timeout(to_timeout(p_timeout)),
         m_timeout_callback(p_timeout_callback), m_loop(this)
 
   {
@@ -284,7 +131,7 @@ protected:
 private:
   struct loop {
 
-    loop(executer_with_timeout_t<t_log> *p_this) : m_this(p_this) {}
+    loop(executer_base_t<t_log> *p_this) : m_this(p_this) {}
 
     ~loop() { m_this->m_thread.join(); }
 
@@ -343,7 +190,7 @@ private:
     }
 
   private:
-    executer_with_timeout_t<t_log> *m_this;
+    executer_base_t<t_log> *m_this;
   };
 
   /// \brief Thread to execute the function that is called if the \p function
@@ -356,174 +203,71 @@ private:
   }
 
 private:
+  std::function<void()> m_function;
+
   timeout m_timeout;
+
+  timeout_callback m_timeout_callback;
+
+  /// \brief Logger object
+  t_log m_log{"executer"};
+
+  /// \brief The loop initiates stopped
+  bool m_stopped{true};
 
   bool m_is_timeout{false};
 
-  timeout_callback m_timeout_callback;
+  bool m_function_returned{true};
+
+  /// \brief Controls the execution of the execution loop
+  std::mutex m_mutex_function;
+
+  /// \brief Controls the execution of the execution loop
+  std::condition_variable m_cond_exec;
+
+  /// \brief Controls the waiting of the execution loop
+  std::mutex m_mutex_wait;
+
+  /// \brief Controls the waiting of the execution loop
+  std::condition_variable m_cond_wait;
+
+  /// \brief Single thread used to execute the \p function over and over
+  /// asynchronously
+  concurrent::thread m_thread;
+
+  /// \brief Controls the execution of the thread
+  std::mutex m_mutex_exec;
 
   std::thread m_timeout_thread;
 
   loop m_loop;
-
-  bool m_function_returned{true};
 };
-
-template <typename t_log, timeout_control, typename t_result,
-          typename... t_params>
-struct executer_t;
-
-// ########## 1 ##########
-template <typename t_log, typename t_result, typename... t_params>
-struct executer_t<t_log, timeout_control::no, t_result, t_params...>;
-
-// ########## 2 ##########
-template <typename t_log, typename t_result>
-struct executer_t<t_log, timeout_control::no, t_result, void>;
-
-// ########## 3 ##########
-template <typename t_log, typename... t_params>
-struct executer_t<t_log, timeout_control::no, void, t_params...>;
-
-// ########## 4 ##########
-template <typename t_log>
-struct executer_t<t_log, timeout_control::no, void, void>;
 
 // ########## 5 ##########
 template <typename t_log, typename t_result, typename... t_params>
-struct executer_t<t_log, timeout_control::yes, t_result, t_params...>;
+struct executer_t;
 
 // ########## 6 ##########
 template <typename t_log, typename t_result>
-struct executer_t<t_log, timeout_control::yes, t_result, void>;
+struct executer_t<t_log, t_result, void>;
 
 // ########## 7 ##########
 template <typename t_log, typename... t_params>
-struct executer_t<t_log, timeout_control::yes, void, t_params...>;
+struct executer_t<t_log, void, t_params...>;
 
 // ########## 8 ##########
-template <typename t_log>
-struct executer_t<t_log, timeout_control::yes, void, void>;
-
-// ########## 1 ##########
-template <typename t_log, typename t_result, typename... t_params>
-struct executer_t<t_log, timeout_control::no, t_result, t_params...>
-    : public executer_no_timeout_t<t_log> {
-
-  typedef std::function<t_result(t_params...)> function;
-
-  executer_t(function p_function)
-      : executer_no_timeout_t<t_log>([this, p_function]() {
-          m_result = std::apply(p_function, std::move(m_params));
-        }) {
-    this->start();
-  }
-
-  std::optional<t_result> operator()(t_params... p_params) {
-
-    auto ok = [this]() -> std::optional<t_result> { return m_result; };
-    auto not_ok = []() -> std::optional<t_result> { return {}; };
-    auto save_params = [this, p_params...]() -> void {
-      m_params = {p_params...};
-    };
-
-    return this->template call_no_timeout<std::optional<t_result>>(ok, not_ok,
-                                                                   save_params);
-  }
-
-private:
-  std::tuple<t_params...> m_params;
-
-  t_result m_result;
-};
-
-// ########## 2 ##########
-template <typename t_log, typename t_result>
-struct executer_t<t_log, timeout_control::no, t_result, void>
-    : public executer_no_timeout_t<t_log> {
-
-  typedef std::function<t_result(void)> function;
-
-  executer_t(function p_function)
-      : executer_no_timeout_t<t_log>(
-            [this, p_function]() { m_result = p_function(); }) {
-    this->start();
-  }
-
-  std::optional<t_result> operator()() {
-
-    auto ok = [this]() -> std::optional<t_result> { return m_result; };
-    auto not_ok = []() -> std::optional<t_result> { return {}; };
-    auto save_params = []() -> void {};
-
-    return this->template call_no_timeout<std::optional<t_result>>(ok, not_ok,
-                                                                   save_params);
-  }
-
-private:
-  t_result m_result;
-};
-
-// ########## 3 ##########
-template <typename t_log, typename... t_params>
-struct executer_t<t_log, timeout_control::no, void, t_params...>
-    : public executer_no_timeout_t<t_log> {
-
-  typedef std::function<void(t_params...)> function;
-
-  executer_t(function p_function)
-      : executer_no_timeout_t<t_log>([this, p_function]() {
-          std::apply(p_function, std::move(m_params));
-        }) {
-    this->start();
-  }
-
-  void operator()(t_params... p_params) {
-    auto ok = [this]() -> void {};
-    auto not_ok = []() -> void {};
-    auto save_params = [this, p_params...]() -> void {
-      m_params = {p_params...};
-    };
-
-    return this->template call_no_timeout<void>(ok, not_ok, save_params);
-  }
-
-private:
-  std::tuple<t_params...> m_params;
-};
-
-// ########## 4 ##########
-template <typename t_log>
-struct executer_t<t_log, timeout_control::no, void, void>
-    : public executer_no_timeout_t<t_log> {
-
-  typedef std::function<void()> function;
-
-  executer_t(function p_function)
-      : executer_no_timeout_t<t_log>([this, p_function]() { p_function(); }) {
-    this->start();
-  }
-
-  void operator()() {
-    auto ok = [this]() -> void {};
-    auto not_ok = []() -> void {};
-    auto save_params = []() -> void {};
-
-    return this->template call_no_timeout<void>(ok, not_ok, save_params);
-  }
-};
+template <typename t_log> struct executer_t<t_log, void, void>;
 
 // ########## 5 ##########
 template <typename t_log, typename t_result, typename... t_params>
-struct executer_t<t_log, timeout_control::yes, t_result, t_params...>
-    : public executer_with_timeout_t<t_log> {
+struct executer_t : public executer_base_t<t_log> {
 
   typedef std::function<t_result(t_params...)> function;
 
   template <typename t_time>
   executer_t(function p_function, t_time p_timeout,
              timeout_callback p_timeout_callback)
-      : executer_with_timeout_t<t_log>(
+      : executer_base_t<t_log>(
             [this, p_function]() -> void {
               m_result = std::apply(p_function, std::move(m_params));
             },
@@ -550,15 +294,14 @@ private:
 
 // ########## 6 ##########
 template <typename t_log, typename t_result>
-struct executer_t<t_log, timeout_control::yes, t_result, void>
-    : public executer_with_timeout_t<t_log> {
+struct executer_t<t_log, t_result, void> : public executer_base_t<t_log> {
 
   typedef std::function<t_result()> function;
 
   template <typename t_time>
   executer_t(function p_function, t_time p_timeout,
              timeout_callback p_timeout_callback)
-      : executer_with_timeout_t<t_log>(
+      : executer_base_t<t_log>(
             [this, p_function]() -> void { m_result = p_function(); },
             p_timeout, p_timeout_callback) {
     this->start();
@@ -580,15 +323,14 @@ private:
 
 // ########## 7 ##########
 template <typename t_log, typename... t_params>
-struct executer_t<t_log, timeout_control::yes, void, t_params...>
-    : public executer_with_timeout_t<t_log> {
+struct executer_t<t_log, void, t_params...> : public executer_base_t<t_log> {
 
   typedef std::function<void(t_params...)> function;
 
   template <typename t_time>
   executer_t(function p_function, t_time p_timeout,
              timeout_callback p_timeout_callback)
-      : executer_with_timeout_t<t_log>(
+      : executer_base_t<t_log>(
             [this, p_function]() -> void {
               std::apply(p_function, std::move(m_params));
             },
@@ -608,17 +350,15 @@ private:
 
 // ########## 8 ##########
 template <typename t_log>
-struct executer_t<t_log, timeout_control::yes, void, void>
-    : public executer_with_timeout_t<t_log> {
+struct executer_t<t_log, void, void> : public executer_base_t<t_log> {
 
   typedef std::function<void()> function;
 
   template <typename t_time>
   executer_t(function p_function, t_time p_timeout,
              timeout_callback p_timeout_callback)
-      : executer_with_timeout_t<t_log>(
-            [this, p_function]() -> void { p_function(); }, p_timeout,
-            p_timeout_callback) {
+      : executer_base_t<t_log>([this, p_function]() -> void { p_function(); },
+                               p_timeout, p_timeout_callback) {
     this->start();
   }
 
--- tst/concurrent/executer/main.cpp
+++ tst/concurrent/executer/main.cpp
@@ -12,267 +12,21 @@ using namespace tenacitas;
 
 typedef logger::cerr::log log;
 
-struct test000 {
-
-  static std::string desc() {
-    return "executer as <concurrent::timeout_control::no, double, int, float>";
-  }
-
-  bool operator()() {
-
-    typedef concurrent::executer_t<log, concurrent::timeout_control::no, double,
-                                   int, float>
-        executer;
-
-    auto function = [this](int p_i, float p_f) -> double {
-      concurrent_debug(m_log, "i = ", p_i, ", f = ", p_f);
-      return p_i * p_f;
-    };
-
-    executer _executer(function);
-
-    int _i = 2;
-    float _f = 3.5;
-
-    std::optional<double> _maybe = _executer(_i, _f);
-
-    if (!_maybe) {
-      concurrent_error(m_log, "nothing returned, but it should have");
-    }
-
-    double _d = std::move(*_maybe);
-
-    if (_d != (_i * _f)) {
-      concurrent_error(m_log, "expected ", _i * _f, ", but got ", _d);
-      return false;
-    }
-
-    concurrent_debug(m_log, "got ", _d, ", as expected");
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    return true;
-  }
-
-private:
-  log m_log{"test000"};
-};
-
-struct test010 {
-
-  static std::string desc() {
-    return "executer as <concurrent::timeout_control::no, double, int>";
-  }
-
-  bool operator()() {
-
-    typedef concurrent::executer_t<log, concurrent::timeout_control::no, double,
-                                   int>
-        executer;
-
-    auto function = [this](int p_i) -> double {
-      concurrent_debug(m_log, "i = ", p_i);
-      return p_i * 2.5;
-    };
-
-    executer _executer(function);
-
-    int _i = 2;
-
-    std::optional<double> _maybe = _executer(_i);
-
-    if (!_maybe) {
-      concurrent_error(m_log, "nothing returned, but it should have");
-    }
-
-    double _d = std::move(*_maybe);
-
-    if (_d != (_i * 2.5)) {
-      concurrent_error(m_log, "expected ", _i * 2.5, ", but got ", _d);
-      return false;
-    }
-
-    concurrent_debug(m_log, "got ", _d, ", as expected");
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    return true;
-  }
-
-private:
-  log m_log{"test010"};
-};
-
-struct test020 {
-
-  static std::string desc() {
-    return "executer as <concurrent::timeout_control::no, double, void>";
-  }
-
-  bool operator()() {
-
-    typedef concurrent::executer_t<log, concurrent::timeout_control::no, double,
-                                   void>
-        executer;
-
-    auto function = []() -> double { return 3.14; };
-
-    executer _executer(function);
-
-    std::optional<double> _maybe = _executer();
-
-    if (!_maybe) {
-      concurrent_error(m_log, "nothing returned, but it should have");
-    }
-
-    double _d = std::move(*_maybe);
-
-    if (_d != (3.14)) {
-      concurrent_error(m_log, "expected ", 3.14, ", but got ", _d);
-      return false;
-    }
-
-    concurrent_debug(m_log, "got ", _d, ", as expected");
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    return true;
-  }
-
-private:
-  log m_log{"test020"};
-};
-
-struct test030 {
-
-  static std::string desc() {
-    return "executer as <concurrent::timeout_control::no, void, int, float>";
-  }
-
-  bool operator()() {
-
-    typedef concurrent::executer_t<log, concurrent::timeout_control::no, void,
-                                   int, float>
-        executer;
-
-    double _d{0};
-
-    auto function = [this, &_d](int p_i, float p_f) -> void {
-      concurrent_debug(m_log, "i = ", p_i, ", f = ", p_f);
-      _d = p_i * p_f;
-    };
-
-    executer _executer(function);
-
-    int _i = 2;
-    float _f = 3.5;
-
-    _executer(_i, _f);
-
-    if (_d != (_i * _f)) {
-      concurrent_error(m_log, "expected ", _i * _f, ", but got ", _d);
-      return false;
-    }
-
-    concurrent_debug(m_log, "got ", _d, ", as expected");
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    return true;
-  }
-
-private:
-  log m_log{"test030"};
-};
-
-struct test040 {
-
-  static std::string desc() {
-    return "executer as <concurrent::timeout_control::no, void, int>";
-  }
-
-  bool operator()() {
-
-    typedef concurrent::executer_t<log, concurrent::timeout_control::no, void,
-                                   int>
-        executer;
-
-    double _d{0};
-
-    auto function = [this, &_d](int p_i) -> void {
-      concurrent_debug(m_log, "i = ", p_i);
-      _d = p_i * 2.5;
-    };
-
-    executer _executer(function);
-
-    int _i = 2;
-
-    _executer(_i);
-
-    if (_d != (_i * 2.5)) {
-      concurrent_error(m_log, "expected ", _i * 2.5, ", but got ", _d);
-      return false;
-    }
-
-    concurrent_debug(m_log, "got ", _d, ", as expected");
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    return true;
-  }
-
-private:
-  log m_log{"test040"};
-};
-
-struct test050 {
-
-  static std::string desc() {
-    return "executer as <concurrent::timeout_control::no, void, void>";
-  }
-
-  bool operator()() {
-
-    typedef concurrent::executer_t<log, concurrent::timeout_control::no, void,
-                                   void>
-        executer;
-
-    double _d{0};
-
-    auto function = [&_d]() -> void { _d = 2.5; };
-
-    executer _executer(function);
-
-    _executer();
-
-    if (_d != 2.5) {
-      concurrent_error(m_log, "expected ", 2.5, ", but got ", _d);
-      return false;
-    }
-
-    concurrent_debug(m_log, "got ", _d, ", as expected");
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    return true;
-  }
-
-private:
-  log m_log{"test050"};
-};
-
 void timeout_callback(std::thread::id p_id) {
-  log _log{"timeout_callback"};
+  log _log{"timeout_callback "};
   concurrent_warn(_log, "timeout for ", p_id);
 }
 
 struct test060 {
 
   static std::string desc() {
-    return "executer as <concurrent::timeout_control::yes, double, int, "
+    return "executer as < double, int, "
            "float>, no timeout";
   }
 
   bool operator()() {
 
-    typedef concurrent::executer_t<log, concurrent::timeout_control::yes,
-                                   double, int, float>
-        executer;
+    typedef concurrent::executer_t<log, double, int, float> executer;
 
     auto function = [this](int p_i, float p_f) -> double {
       concurrent_debug(m_log, "i = ", p_i, ", f = ", p_f);
@@ -313,15 +67,13 @@ private:
 struct test070 {
 
   static std::string desc() {
-    return "executer as <concurrent::timeout_control::yes, double, int>, no "
+    return "executer as < double, int>, no "
            "timeout";
   }
 
   bool operator()() {
 
-    typedef concurrent::executer_t<log, concurrent::timeout_control::yes,
-                                   double, int>
-        executer;
+    typedef concurrent::executer_t<log, double, int> executer;
 
     auto function = [this](int p_i) -> double {
       concurrent_debug(m_log, "i = ");
@@ -358,7 +110,7 @@ private:
 struct test071 {
 
   static std::string desc() {
-    return "executer as <concurrent::timeout_control::yes, double, int>, with "
+    return "executer as < double, int>, with "
            "timeout, and function returned before the next call, which did not "
            "timeout";
   }
@@ -368,9 +120,7 @@ struct test071 {
     const std::chrono::seconds _timeout{1};
     std::chrono::milliseconds _sleep{2000};
 
-    typedef concurrent::executer_t<log, concurrent::timeout_control::yes,
-                                   double, int>
-        executer;
+    typedef concurrent::executer_t<log, double, int> executer;
 
     auto _function = [this, &_sleep](int p_i) -> double {
       concurrent_debug(m_log, "i = ", p_i, ", and sleep = ", _sleep.count(),
@@ -428,7 +178,7 @@ private:
 struct test072 {
 
   static std::string desc() {
-    return "executer as <concurrent::timeout_control::yes, double, int>, with "
+    return "executer as < double, int>, with "
            "timeout, and function did not returned before the next call, which "
            "did not timeout";
   }
@@ -438,9 +188,7 @@ struct test072 {
     const std::chrono::seconds _timeout{1};
     std::chrono::milliseconds _sleep{4000};
 
-    typedef concurrent::executer_t<log, concurrent::timeout_control::yes,
-                                   double, int>
-        executer;
+    typedef concurrent::executer_t<log, double, int> executer;
 
     auto _function = [this, &_sleep](int p_i) -> double {
       concurrent_debug(m_log, "i = ", p_i, ", and sleep = ", _sleep.count(),
@@ -498,12 +246,6 @@ int main(int argc, char **argv) {
 
   tenacitas::logger::cerr::log::set_debug();
   tenacitas::tester::test _test(argc, argv);
-  run_test(_test, test000);
-  run_test(_test, test010);
-  run_test(_test, test020);
-  run_test(_test, test030);
-  run_test(_test, test040);
-  run_test(_test, test050);
   run_test(_test, test060);
   run_test(_test, test070);
   run_test(_test, test071);
