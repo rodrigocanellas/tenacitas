77e10d143164627bb418b73950de3c75326aef1e
Rodrigo Canellas <rodrigo.canellas@psafe.com>
2021-06-04 18:36:17 -0300

    improving 'dispatcher' example

--- exp/tenacitas/async/dispatcher_000/main.cpp
+++ exp/tenacitas/async/dispatcher_000/main.cpp
@@ -1,148 +1,9 @@
-///// \copyright This file is under GPL 3 license. Please read the \p LICENSE
-/// file
-///// at the root of \p tenacitas directory
-
-///// \author Rodrigo Canellas - rodrigo.canellas at gmail.com
-
-///// \example messenger_000
-
-//#include <chrono>
-//#include <condition_variable>
-//#include <cstdint>
-//#include <functional>
-//#include <iostream>
-//#include <mutex>
-
-//#include <tenacitas/async.h>
-//#include <tenacitas/number.h>
-//#include <tenacitas/type.h>
-
-// using namespace tenacitas;
-// using namespace std::chrono_literals;
-
-//// message to be sent
-// struct temperature {
-//    explicit temperature(uint16_t p_idx = 0, float p_value = 0.0)
-//        : idx(p_idx)
-//        , value(p_value) {}
-
-//    friend std::ostream &operator<<(std::ostream &p_out,
-//                                    const temperature &p_temperature) {
-//        p_out << '[' << p_temperature.idx << ',' << p_temperature.value <<
-//        ']'; return p_out;
-//    }
-
-//    uint16_t idx {0};
-//    float value {0.0};
-//};
-
-//// simulates a temperature sensor generating values
-// struct temperature_sensor {
-//    temperature_sensor(uint16_t p_max)
-//        : m_max(p_max)
-//        , m_temperature_generator(
-//              m_id,
-//              [this](type::ptr<bool>) { generator(); },
-//              m_timeout,
-//              m_interval) {}
-
-//    // starts to generate temperatures
-//    void start() { m_temperature_generator.start(); }
-
-//    //    ~temperature_sensor() { m_temperature_generator.stop(); }
-
-// private:
-//    // type for the asynchronous loop that will call the 'generator' method
-//    typedef async::sleeping_loop temperature_generator;
-
-// private:
-//    // function to be executed inside the sleeping loop, that will generate
-//    the
-//    // temperatures
-//    void generator() {
-//        if (m_counter >= m_max) {
-//            if (!m_reported) {
-//                std::cout << "all temperatures generated" << std::endl;
-//                m_reported = true;
-//            }
-//            return;
-//        }
-//        m_temperature += 0.2;
-//        async::dispatch(temperature {++m_counter, m_temperature});
-//    }
-
-// private:
-//    // counter which will be incremented by the function executed in the
-//    // sleeping loop
-//    uint16_t m_counter {0};
-
-//    // max number of temperatures value of the counter
-//    const uint16_t m_max;
-
-//    // temperature to be sent
-//    float m_temperature = 23.1;
-
-//    // timeout for the function executed in the sleeping loop
-//    const std::chrono::seconds m_timeout {1s};
-
-//    // interval of execution of the sleeping loop
-//    const std::chrono::milliseconds m_interval {500ms};
-
-//    // asynchronous loop that will generate temperature at each m_interval
-//    temperature_generator m_temperature_generator;
-
-//    // to avoid writing more than one time that all the temperatures were
-//    // generated
-//    bool m_reported {false};
-
-//    number::id m_id;
-//};
-
-// struct temperature_handler {
-//    temperature_handler(uint16_t p_max)
-//        : m_max(p_max) {}
-
-//    void operator()(type::ptr<bool>, temperature &&p_temperature) {
-
-//        std::cout << p_temperature << std::endl;
-//        std::this_thread::sleep_for(1s);
-
-//        if (p_temperature.idx >= m_max) {
-//            return;
-//        }
-//    }
-
-// private:
-//    // number of temperatures to be generated
-//    const uint16_t m_max;
-//};
-
-// int main() {
-
-//    const uint16_t _max {20};
-
-//    // adding the function that will handle temperatures to an unamed handling
-//    // of temperature handlers; 2s is the time _temperature_handler has to
-//    // execute
-//    async::add_handler<temperature>(temperature_handler {_max}, 2s);
-
-//    // declaring the temperature sensor
-//    temperature_sensor _sensor {_max};
-
-//    // starting the sensor
-//    _sensor.start();
-
-//    std::cout << "Press 'x' to exit: ";
-//    char _key;
-//    std::cin >> _key;
-//}
-
 /// \copyright This file is under GPL 3 license. Please read the \p LICENSE file
 /// at the root of \p tenacitas directory
 
 /// \author Rodrigo Canellas - rodrigo.canellas at gmail.com
 
-/// \example messenger_000
+/// \example dispatcher_000
 
 #include <chrono>
 #include <condition_variable>
@@ -153,18 +14,16 @@
 
 #include <tenacitas/async.h>
 #include <tenacitas/number.h>
-//#include <tenacitas/type.h>
+#include <tenacitas/type.h>
 
 using namespace tenacitas;
 using namespace std::chrono_literals;
 
 // message to be sent
 struct temperature {
-    explicit temperature(float p_value = 0.0)
-        : value(p_value) {}
+    explicit temperature(float p_value = 0.0) : value(p_value) {}
 
-    friend std::ostream &operator<<(std::ostream &p_out,
-                                    const temperature &p_temperature) {
+    friend std::ostream& operator<<(std::ostream& p_out, const temperature& p_temperature) {
         p_out << p_temperature.value;
         return p_out;
     }
@@ -174,16 +33,18 @@ struct temperature {
 
 // simulates a temperature sensor generating values
 struct temperature_sensor {
-    temperature_sensor(uint16_t p_max)
-        : m_max(p_max)
-        , m_temperature_generator(
-              m_id,
-              [this](type::ptr<bool>) { generator(); },
-              m_timeout,
-              m_interval) {}
+    temperature_sensor(uint16_t p_max) :
+        m_max(p_max),
+        m_temperature_generator(
+            m_id,
+            [this](type::ptr<bool>) { generator(); },
+            m_timeout,
+            m_interval) {}
 
     // starts to generate temperatures
-    void start() { m_temperature_generator.start(); }
+    void start() {
+        m_temperature_generator.start();
+    }
 
 private:
     // type for the asynchronous loop that will call the 'generator' method
@@ -232,8 +93,26 @@ private:
     number::id m_id;
 };
 
-int main() {
+struct temperature_handler {
+    temperature_handler(uint16_t p_max, uint16_t* p_counter_received, std::condition_variable* p_cond) :
+        m_max(p_max), m_counter_received(p_counter_received), m_cond(p_cond) {}
 
+    void operator()(type::ptr<bool>, temperature&& p_temperature) {
+        std::cout << ++(*m_counter_received) << " - " << p_temperature << std::endl;
+        std::this_thread::sleep_for(1s);
+
+        if ((*m_counter_received) >= m_max) {
+            m_cond->notify_one();
+        }
+    }
+
+private:
+    uint16_t m_max {0};
+    uint16_t* m_counter_received;
+    std::condition_variable* m_cond;
+};
+
+int main() {
     // condition variable and mutex to wait for the sleeping loop to finish
     std::condition_variable _cond;
     std::mutex _mutex;
@@ -244,21 +123,19 @@ int main() {
     // counter of temperatures received
     uint16_t _counter_received {0};
 
-    // function that will handle temperatures sent
-    auto _temperature_handler = [&_counter_received,
-                                 &_cond](type::ptr<bool>,
-                                         temperature &&p_temperature) -> void {
-        std::cout << ++_counter_received << " - " << p_temperature << std::endl;
-        std::this_thread::sleep_for(1s);
+    //    // function that will handle temperatures sent
+    //    auto _temperature_handler = [&_counter_received, &_cond](type::ptr<bool>, temperature&& p_temperature) -> void {
+    //        std::cout << ++_counter_received << " - " << p_temperature << std::endl;
+    //        std::this_thread::sleep_for(1s);
 
-        if (_counter_received >= _max) {
-            _cond.notify_one();
-        }
-    };
+    //        if (_counter_received >= _max) {
+    //            _cond.notify_one();
+    //        }
+    //    };
 
     // adding the function that will handle temperatures to an unamed queue of
     // temperature handlers; 2s is the time _temperature_handler has to execute
-    async::add_handler<temperature>(_temperature_handler, 2s);
+    async::add_handler<temperature>(temperature_handler {_max, &_counter_received, &_cond}, 2s);
 
     // declaring the temperature sensor
     temperature_sensor _sensor {_max};
